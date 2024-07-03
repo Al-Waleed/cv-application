6f3362cf7c58f963280a2b761da42d91ee388748
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-07-03 19:31:10 +0300

    Create an App component and use it in main.jsx instead of using the other components directly in main.jsx also we'll use it to create shared states between the other components

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import GeneralInfo from "./general-info";
+import EducationalExp from "./educational-exp";
+import PracticalExp from "./practical-exp";
+
+export default function App() {
+  return (
+    <>
+      <GeneralInfo />
+      <EducationalExp />
+      <PracticalExp />
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GeneralInfo from './components/general-info'
-import EducationalExp from './components/educational-exp'
-import PracticalExp from './components/practical-exp'
+import App from './components/app';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GeneralInfo />
-    <EducationalExp />
-    <PracticalExp />
+    <App />
   </React.StrictMode>
 );
