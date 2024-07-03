041ebae7b1d1927eafe3585222291db30f699f70
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-07-03 18:46:52 +0300

    Add props to EducationalExp()

--- src/components/educational-exp.jsx
+++ src/components/educational-exp.jsx
@@ -1,18 +1,40 @@
 import "../styles/educational-exp.css";
 
-export default function EducationalExp() {
+export default function EducationalExp({
+  schoolValue,
+  changeSchool,
+  studyTitleValue,
+  changeStudyTitle,
+  studyDateValue,
+  changeStudyDate,
+}) {
   return (
     <div id="educational-exp">
       <h1>Educational Experience</h1>
 
       <label htmlFor="school">School name: </label>
-      <input type="text" id="school" />
+      <input
+        type="text"
+        id="school"
+        value={schoolValue}
+        onChange={changeSchool}
+      />
 
       <label htmlFor="study-title">Title Of Study: </label>
-      <input type="text" id="study-title" />
+      <input
+        type="text"
+        id="study-title"
+        value={studyTitleValue}
+        onChange={changeStudyTitle}
+      />
 
       <label htmlFor="study-date">Date of study: </label>
-      <input type="date" id="study-date" />
+      <input
+        type="date"
+        id="study-date"
+        value={studyDateValue}
+        onChange={changeStudyDate}
+      />
 
       <button type="submit">Submit</button>
       <button>Edit</button>
