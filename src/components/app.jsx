957c7e558c4182261a3214d0d4491bdaa23719ad
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-07-03 19:50:32 +0300

    Add useStates and callback functions to handle each useState for all component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,78 @@
+import { useState } from "react";
 import GeneralInfo from "./general-info";
 import EducationalExp from "./educational-exp";
 import PracticalExp from "./practical-exp";
 
 export default function App() {
+  // states for GeneraInfo()
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  // states for EducationalExp()
+  const [school, setSchool] = useState("");
+  const [studyTitle, setStudyTitle] = useState("");
+  const [studyDate, setStudyDate] = useState("");
+  // states for PracticalExp()
+  const [company, setCompany] = useState("");
+  const [position, setPosition] = useState("");
+  const [responsibilities, setResponsibilities] = useState("");
+
+  // GeneralInfo() states handle function
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+  function handlePhoneChange(e) {
+    setPhone(e.target.value);
+  }
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+  // EducationalExp() states handle function
+  function handleSchoolChange(e) {
+    setSchool(e.target.value);
+  }
+  function handleStudyTitleChange(e) {
+    setStudyTitle(e.target.value);
+  }
+  function handleStudyDateChange(e) {
+    setStudyDate(e.target.value);
+  }
+  // PracticalExp() states handle function
+  function handleCompanyChange(e) {
+    setCompany(e.target.value);
+  }
+  function handlePositionChange(e) {
+    setPosition(e.target.value);
+  }
+  function handleResponsibilitiesChange(e) {
+    setResponsibilities(e.target.value);
+  }
   return (
     <>
-      <GeneralInfo />
-      <EducationalExp />
-      <PracticalExp />
+      <GeneralInfo
+        nameValue={name}
+        changeName={handleNameChange}
+        phoneValue={phone}
+        changePhone={handlePhoneChange}
+        emailValue={email}
+        changeEmail={handleEmailChange}
+      />
+      <EducationalExp
+        schoolValue={school}
+        changeSchool={handleSchoolChange}
+        studyTitleValue={studyTitle}
+        changeStudyTitle={handleStudyTitleChange}
+        studyDateValue={studyDate}
+        changeStudyDate={handleStudyDateChange}
+      />
+      <PracticalExp
+        company={company}
+        changeCompany={handleCompanyChange}
+        position={position}
+        changePosition={handlePositionChange}
+        responsibilities={responsibilities}
+        changeResponsibilities={handleResponsibilitiesChange}
+      />
     </>
   );
 }
