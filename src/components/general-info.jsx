e3c5f9c8e9da1a46f0134d3f9ddac6c91e057a28
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-07-03 18:41:50 +0300

    Add props to GeneralInfo()

--- src/components/general-info.jsx
+++ src/components/general-info.jsx
@@ -1,18 +1,35 @@
 import "../styles/general-info.css";
 
-export default function GeneralInfo() {
+export default function GeneralInfo({
+  nameValue,
+  changeName,
+  emailValue,
+  changeEmail,
+  phoneValue,
+  changePhone,
+}) {
   return (
     <div id="general-info">
       <h1>General Information</h1>
 
       <label htmlFor="name">Name: </label>
-      <input type="text" id="name" />
+      <input type="text" id="name" value={nameValue} onChange={changeName} />
 
       <label htmlFor="email">Email: </label>
-      <input type="email" id="email" />
+      <input
+        type="email"
+        id="email"
+        value={emailValue}
+        onChange={changeEmail}
+      />
 
       <label htmlFor="phone">Phone number: </label>
-      <input type="number" id="phone" />
+      <input
+        type="number"
+        id="phone"
+        value={phoneValue}
+        onChange={changePhone}
+      />
 
       <button type="submit">Submit</button>
       <button>Edit</button>
