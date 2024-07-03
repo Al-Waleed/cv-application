c261d7118dc3c8ba6671c173abb1f9c6ecdda771
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-07-03 18:50:09 +0300

    Add props to PracticalExp()

--- src/components/practical-exp.jsx
+++ src/components/practical-exp.jsx
@@ -1,23 +1,45 @@
 import "../styles/practical-exp.css";
 
-export default function PracticalExp() {
+export default function PracticalExp({
+  company,
+  changeCompany,
+  position,
+  changePosition,
+  responsibilities,
+  changeResponsibilities,
+}) {
   return (
     <div id="practical-exp">
       <h1>Practical Experience</h1>
-  
+
       <label htmlFor="company">Company name: </label>
-      <input type="text" id="company" />
+      <input
+        type="text"
+        id="company"
+        value={company}
+        onChange={changeCompany}
+      />
 
       <label htmlFor="position">Position title: </label>
-      <input type="text" id="position" />
+      <input
+        type="text"
+        id="position"
+        value={position}
+        onChange={changePosition}
+      />
 
       <label htmlFor="responsibilities">
         Main responsibilities of your job:
       </label>
-      <textarea type="text" id="responsibilities" />
+      <textarea
+        type="text"
+        id="responsibilities"
+        value={responsibilities}
+        onChange={changeResponsibilities}
+      />
 
       <button type="submit">Submit</button>
-      <button >Edit</button>
+      <button>Edit</button>
     </div>
   );
 }
